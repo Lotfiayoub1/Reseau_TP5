d1d7184225f9b784555c4c8c5049e455ac55693e
Lotfiayoub1 <63500139+Lotfiayoub1@users.noreply.github.com>
2021-11-18 22:28:30 +0100

    Final touch

--- src/com/reseau/Server.java
+++ src/com/reseau/Server.java
@@ -1,67 +1,77 @@
 package com.reseau;
+
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Server {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket server = new ServerSocket(9997);
+
         try {
-            //cr�ation du socket serveur:
-            ServerSocket listener = new ServerSocket(9997);
+            while(true) {
+                System.out.println("\nwaiting for a new connection.....\n");
+                Socket client_sock = server.accept();
 
-            try {
-                while ( true )
-                {
-                    System.out.println("\nwaiting for a new connection.....\n");
-                    //attente de connexion d'un client ( les connexion are queued in the port )
-                    Socket client_sock = listener.accept();
-                    try
-                    {
-                        //creation des flots de lecture et d'�criture ASCII
-                        Scanner in = new Scanner( client_sock.getInputStream()) ;
-                        PrintWriter out = new PrintWriter(client_sock.getOutputStream(), true) ;
+                InputStreamReader in = new InputStreamReader(client_sock.getInputStream());
+                BufferedReader bf = new BufferedReader(in);
+                String str = bf.readLine();
+                System.out.println("<SERVER> From the client the menu number : "+ str);
 
-                        out.println("Merhbaa bik f serveur dyalna!!! ");
+                if (Integer.parseInt(str) == 1){
 
-                        //wait something from the client
-                        String line= null;
+                    PrintWriter out = new PrintWriter(client_sock.getOutputStream());
+                    out.println("Le serveur Va calculer la longuer de la phrase que tu va entrer!!!" );
+                    out.flush();
+                    //System.out.println("Client connected");
+                    System.out.println("<SERVER> waiting for client input ");
 
-                        System.out.println("<SERVER> waiting for client input ");
-
-                        try {
-                            //while ( !( line = in.nextLine() ).isEmpty())
-                            line = in.nextLine();
-                            System.out.println("<SERVER> from the client : " + line);
-                            System.out.println("<SERVER> from the client : " + line.toUpperCase());
-                            //PrintWriter out2 = new PrintWriter(client_sock.getOutputStream(), true) ;
-                            //out2.print(line.toUpperCase());
-                        }catch( Exception e) {
-                            //System.out.println(e);
-                        }
-                        Scanner S = new Scanner(System.in);
-                        String toto = S.nextLine();
-                        out.println(toto);
-                        out.flush();
-                        System.out.println("<SERVER>: dakchi mcha");
+                    BufferedReader bf2 = new BufferedReader(in);
+                    String str2 = bf2.readLine();
+                    System.out.println("<SERVER> From the client : "+ str2);
 
 
-
-                    }finally {
-                        System.out.println("closing.....");
-                        client_sock.close();
-                    }
+                    out.println("la longueur de la phrase est "+ str2.length());
+                    out.flush();
 
 
+                    client_sock.close();
+                }else{
+                    PrintWriter out = new PrintWriter(client_sock.getOutputStream());
+                    out.println("Le serveur Va trier la phrase!!!" );
+                    out.flush();
+                    //System.out.println("Client connected");
+                    System.out.println("<SERVER> waiting for client input ");
+
+                    BufferedReader bf2 = new BufferedReader(in);
+                    String str2 = bf2.readLine();
+                    System.out.println("<SERVER> From the client : "+ str2);
+
+                    char[] StringtoChar = str2.toCharArray();
+                    Arrays.sort(StringtoChar);
+                    String SortedString = new String(StringtoChar);
+                    out.println("The Sorted String is : " + SortedString);
+                    //System.out.println("The Unsorted String is : " + str2);
+                    //System.out.println("The Sorted String is : " + SortedString);
+                    out.flush();
+
+
+                    client_sock.close();
                 }
-            }finally
-            {
-                listener.close();
+
+
             }
-        } catch (IOException e) {}
+        } finally {
+            server.close();
+        }
+
 
     }
 
